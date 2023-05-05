67587fd7bb45fe66512d9a8da4b9cb40067aa7f9
raul <raul@pc217-11.14005298.14.andared.ced.junta-andalucia.es>
2023-05-05 10:30:06 +0200

    Finalización punto 2. Filtrado e inicio punto 3. Ordenación

---
+++ ProyectoLibreriaClinica/src/dam/main/ColumnOrder.java
@@ -0,0 +1,9 @@
+package dam.main;
+
+public class ColumnOrder {
+
+	public ColumnOrder() {}
+	
+	public final static int ASC =1;
+	public final static int DESC =2;
+}
--- ProyectoLibreriaClinica/src/dam/main/DataBaseManager.java
+++ ProyectoLibreriaClinica/src/dam/main/DataBaseManager.java
@@ -5,9 +5,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
 import java.util.ArrayList;
+import java.util.HashMap;
 /**
- * Gestor de conexión a la bbdd
+ * Gestor de conexión a la bbdd, contiene métodos para realizar el filtrado, la selección de columnas, 
+ * la modificación, borrado o añadido de datos...
  * @author Raul  
  * @version 1.0
  */
@@ -142,22 +145,207 @@ public class DataBaseManager {
 	}
 
 	/**
-	 * Metodo para obtener los datos de una consulta de manera ordenada por alguno de los campos seleccionados
+	 * Método para obtener un filtro de las columnas de cualquier tabla
+	 * @param table indica la tabla de la bbdd de donde se extraen los datos
+	 * @param fields es una array que indica el numero de columnas de las que se quieren extraer datos 
+	 * @filters filters indica el valor por el que se va a comparar
+	 * @return devuelve una ArrayList con los datos extraidos de la bbdd
 	 */
-	public ArrayList<Queryable> findOrderBy (Queryable table){
+	public ArrayList<Queryable> findAllBy(Queryable table, HashMap<String, Object> filters, String...fields){
 		ArrayList<Queryable> result=null;
 		Queryable datos=null;
-
-		return null;
+		int type, j=0;
+		String peticion = "SELECT ";
+		String whereData="";
+		if (fields.length == 0) {
+			peticion += "* ";
+		} else {
+			for(String field : fields) {
+				peticion += field + ",";
+			}
+		}
+		peticion.substring(0, peticion.length()-1);
+		try {
+		for	(String key : filters.keySet()) {
+			whereData+= key+"=? AND ";
+		}
+		whereData = whereData.substring(0, whereData.length()-5);
+			PreparedStatement ps = this.connection.prepareStatement(peticion + " FROM " + 
+					table.getTabla() + " where " + whereData);
+			
+			for(Object filter: filters.values()) {
+				if (filter instanceof Integer) {
+					type = Types.INTEGER;
+				} else if(filter instanceof String) {
+					type = Types.VARCHAR;
+				} else if (filter instanceof Double) {
+					type = Types.DOUBLE;
+				}
+			}	
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				if(table.getTabla().equals("Paciente")) {
+					datos = new Paciente();
+					for(int i=0;i<fields.length;i++) {
+						if(fields[i].toLowerCase().equals("idpaciente")) {
+							((Paciente)datos).setIdPaciente(rs.getInt(i+1));
+						}else if(fields[i].toLowerCase().equals("nombre")) {
+							((Paciente)datos).setNombre(rs.getString(i+1));
+						}else if(fields[i].toLowerCase().equals("edad")) {
+							((Paciente)datos).setEdad(rs.getInt(i+1));
+						}							
+					}
+					//validación de campo clave
+					if(((Paciente)datos).getIdPaciente()==0) continue;
+				} else if (table.getTabla().equals("Fisioterapeuta")) {
+					datos = new Fisioterapeuta();
+					for(int i=0;i<fields.length;i++) {
+						if(fields[i].toLowerCase().equals("idfisio")) {
+							((Fisioterapeuta)datos).setIdFisio(rs.getInt(i+1));
+						} else if (fields[i].toLowerCase().equals("nombre")){
+							((Fisioterapeuta)datos).setNombre(rs.getString(i+1));
+						} else if (fields[i].toLowerCase().equals("especialidad")){
+							((Fisioterapeuta)datos).setEspecialidad(rs.getString(i+1));
+						}else if (fields[i].toLowerCase().equals("localidad")){
+							((Fisioterapeuta)datos).setLocalidad(rs.getString(i+1));
+						}
+					}
+					//validación de campo clave
+					if(((Fisioterapeuta)datos).getIdFisio()==0) continue;
+				} else if (table.getTabla().equals("Diagnostico")) {
+					datos = new Diagnostico();
+					for(int i=0;i<fields.length;i++) {
+						if(fields[i].toLowerCase().equals("fecha")) {
+							((Diagnostico)datos).setFecha(rs.getDate(i+1).toLocalDate());
+						} else if (fields[i].toLowerCase().equals("iddiagfisio")) {
+							((Diagnostico)datos).setIdDiagFisio(rs.getInt(i+1));
+						} else if (fields[i].toLowerCase().equals("iddiagpac")) {
+							((Diagnostico)datos).setIdDiagPac(rs.getInt(i+1));
+						} else if (fields[i].toLowerCase().equals("descripcion")) {
+							((Diagnostico)datos).setDescripcion(rs.getString(i+1));
+						}
+					}
+					//validación de campo clave
+					if(((Diagnostico)datos).getFecha()==null) continue;
+				}
+				result.add(datos);
+			}
+		} catch (SQLException e) {			
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	/**
+	 * Metodo para obtener los datos de una consulta de manera ordenada por alguno de los campos seleccionados
+	 */
+	public ArrayList<Queryable> findOrderBy (Queryable table,ColumnOrder columnOrder, String... fields){
+		ArrayList<Queryable> result=null;
+		Queryable datos=null;
+		String peticion = "SELECT ";
+		String orderBy = " ORDER BY ";
+		if (fields.length == 0) {
+			peticion += "* ";
+		} else {
+			for(String field : fields) {
+				peticion += field + ",";
+			}
+		}
+		peticion.substring(0, peticion.length()-1);
+		
+		try {
+			PreparedStatement ps = this.connection.prepareStatement(peticion + " FROM " + 
+					table.getTabla() + orderBy );
+			ResultSet rs = ps.executeQuery();					
+			while(rs.next()) {
+				if(table.getTabla().equals("Paciente")) {
+					datos = new Paciente();
+					for(int i=0;i<fields.length;i++) {
+						if(fields[i].toLowerCase().equals("idpaciente")) {
+							((Paciente)datos).setIdPaciente(rs.getInt(i+1));
+						}else if(fields[i].toLowerCase().equals("nombre")) {
+							((Paciente)datos).setNombre(rs.getString(i+1));
+						}else if(fields[i].toLowerCase().equals("edad")) {
+							((Paciente)datos).setEdad(rs.getInt(i+1));
+						}							
+					}
+					//validación de campo clave
+					if(((Paciente)datos).getIdPaciente()==0) continue;
+				} else if (table.getTabla().equals("Fisioterapeuta")) {
+					datos = new Fisioterapeuta();
+					for(int i=0;i<fields.length;i++) {
+						if(fields[i].toLowerCase().equals("idfisio")) {
+							((Fisioterapeuta)datos).setIdFisio(rs.getInt(i+1));
+						} else if (fields[i].toLowerCase().equals("nombre")){
+							((Fisioterapeuta)datos).setNombre(rs.getString(i+1));
+						} else if (fields[i].toLowerCase().equals("especialidad")){
+							((Fisioterapeuta)datos).setEspecialidad(rs.getString(i+1));
+						}else if (fields[i].toLowerCase().equals("localidad")){
+							((Fisioterapeuta)datos).setLocalidad(rs.getString(i+1));
+						}
+					}
+					//validación de campo clave
+					if(((Fisioterapeuta)datos).getIdFisio()==0) continue;
+				} else if (table.getTabla().equals("Diagnostico")) {
+					datos = new Diagnostico();
+					for(int i=0;i<fields.length;i++) {
+						if(fields[i].toLowerCase().equals("fecha")) {
+							((Diagnostico)datos).setFecha(rs.getDate(i+1).toLocalDate());
+						} else if (fields[i].toLowerCase().equals("iddiagfisio")) {
+							((Diagnostico)datos).setIdDiagFisio(rs.getInt(i+1));
+						} else if (fields[i].toLowerCase().equals("iddiagpac")) {
+							((Diagnostico)datos).setIdDiagPac(rs.getInt(i+1));
+						} else if (fields[i].toLowerCase().equals("descripcion")) {
+							((Diagnostico)datos).setDescripcion(rs.getString(i+1));
+						}
+					}
+					//validación de campo clave
+					if(((Diagnostico)datos).getFecha()==null) continue;
+				}
+				result.add(datos);
+			}
+		} catch (SQLException e) {			
+			e.printStackTrace();
+		}
+		return result;
 	}
 	/**
 	 * Se debe poder modificar cualquiera de los registros de una tabla
 	 */
-	public ArrayList<Queryable> modify (Queryable table){
+	public ArrayList<Queryable> updateValue (Queryable table){
 		ArrayList<Queryable> result=null;
 		Queryable datos=null;
-		
-		return null;
+		String changes="";
+		changes = "UPDATE ";
+		try {
+			PreparedStatement ps = this.connection.prepareStatement("SELECT * FROM " + 
+					table.getTabla());
+			ResultSet rs = ps.executeQuery();					
+			while(rs.next()) {
+				if(table.getTabla().equals("Paciente")) {
+					datos = new Paciente();
+					((Paciente)datos).setIdPaciente(rs.getInt(1));
+					((Paciente)datos).setNombre(rs.getString(2));
+					((Paciente)datos).setEdad(rs.getInt(3));
+				} else if (table.getTabla().equals("Fisioterapeuta")) {
+					datos = new Fisioterapeuta();
+					((Fisioterapeuta)datos).setIdFisio(rs.getInt(1));
+					((Fisioterapeuta)datos).setNombre(rs.getString(2));
+					((Fisioterapeuta)datos).setEspecialidad(rs.getString(3));
+					((Fisioterapeuta)datos).setLocalidad(rs.getString(4));
+				} else if (table.getTabla().equals("Diagnostico")) {
+					datos = new Diagnostico();
+					((Diagnostico)datos).setFecha(rs.getDate(1).toLocalDate());
+					((Diagnostico)datos).setIdDiagFisio(rs.getInt(2));
+					((Diagnostico)datos).setIdDiagPac(rs.getInt(3));
+					((Diagnostico)datos).setDescripcion(rs.getString(4));
+				}
+				result.add(datos);
+			}
+		} catch (SQLException e) {			
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 
